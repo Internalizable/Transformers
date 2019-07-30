a63dc0529e44b373107f2b82bc3de2bd90f59148
Internalizable <elietgm1@gmail.com>
2019-07-30 12:17:18 +0300

    First Commit.

---
+++ src/main/java/me/elie/transformers/Transformers.java
@@ -0,0 +1,17 @@
+package me.elie.transformers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Transformers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
